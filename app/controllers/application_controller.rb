7cef1c0f90df11a4c5da8508cef94f61028f99cf
Chris Pinder <kcpinder90@gmail.com>
2016-05-23 13:05:47 -0600

    make app production ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+# laymans: before any action performed by controller, we need a authenticated user
+  before_action :authenticate_user!
 end
